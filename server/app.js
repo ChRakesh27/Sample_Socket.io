e37f86254a6e03319cc87b368d21251ee1aeff56
Rakesh <chipparakesh01@gmail.com>
2023-05-01 15:26:06 +0530

    ewad

--- server/app.js
+++ server/app.js
@@ -12,9 +12,9 @@ app.use(cors())
 
 const Port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname + "/build/index.html"))
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"))
 );
 
 
