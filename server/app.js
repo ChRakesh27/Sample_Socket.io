513f15ad7242daf6636d22fde9ade7de65d5dd08
Rakesh <chipparakesh01@gmail.com>
2023-05-01 15:14:10 +0530

    update

--- server/app.js
+++ server/app.js
@@ -1,16 +1,22 @@
 
 const express = require('express');
 const cors = require('cors');
+const dotenv = require("dotenv");
 const path = require('path');
 const { Socket } = require('socket.io');
 
+dotenv.config();
 const app = express();
 
 app.use(cors())
 
-const Port = 5000;
+const Port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '/build')));
+app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname + "/build/index.html"))
+);
+
 
 const server = app.listen(Port, console.log(`server run port ${Port}`))
 
